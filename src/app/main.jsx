2f00fa3e7b2d6a7c152d21948ae6bfb035adbe1a
makesomelayouts <makesomelayouts@gmail.com>
2025-10-04 00:43:51 +0400

    Init router

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@app/providers/index.css";
-import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 );
