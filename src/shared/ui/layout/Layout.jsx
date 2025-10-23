6e30007e6aa3203d6e9130876d6a993e06d54f74
makesomelayouts <makesomelayouts@gmail.com>
2025-10-23 10:39:15 +0400

    Dont restrict scroll

--- src/shared/ui/layout/Layout.jsx
+++ src/shared/ui/layout/Layout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router";
 function Layout() {
   return (
     <main className="bg-[#010207] flex flex-col font-unbounded">
-      <div className="h-dvh snap-y snap-mandatory overflow-y-auto">
-        <Outlet />
-      </div>
+      {/* <div className="h-dvh snap-y snap-mandatory overflow-y-auto">*/}
+      <Outlet />
+      {/* </div>*/}
     </main>
   );
 }
