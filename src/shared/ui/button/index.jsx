2592d2b0f5449dde36e833a64bd0ab70b8c65c5e
makesomelayouts <makesomelayouts@gmail.com>
2025-10-25 19:34:00 +0400

    Button: Add hover and active states

--- src/shared/ui/button/index.jsx
+++ src/shared/ui/button/index.jsx
@@ -5,7 +5,7 @@ function Button({ text, link }) {
     <>
       {link ? (
         <Link to={link}>
-          <button className="bg-[#BC13FE] text-2xl rounded-xl py-[7px] px-[38px] text-[#F5E2FF] cursor-pointer">
+          <button className="bg-[#BC13FE] hover:text-[#7B279C] active:text-[#4A0863] text-2xl rounded-xl py-[7px] px-[38px] text-[#F5E2FF] cursor-pointer">
             {text}
           </button>
         </Link>
