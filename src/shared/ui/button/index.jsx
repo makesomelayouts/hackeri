e9afc5517e9c567fad9015623e3d468734163fea
makesomelayouts <makesomelayouts@gmail.com>
2025-10-27 03:49:20 +0400

    Button: Fix hover and active states (from text to bg)

--- src/shared/ui/button/index.jsx
+++ src/shared/ui/button/index.jsx
@@ -5,7 +5,7 @@ function Button({ text, link }) {
     <>
       {link ? (
         <Link to={link}>
-          <button className="bg-[#BC13FE] hover:text-[#7B279C] active:text-[#4A0863] text-2xl rounded-xl py-[7px] px-[38px] text-[#F5E2FF] cursor-pointer">
+          <button className="bg-[#BC13FE] hover:bg-[#7B279C] active:bg-[#4A0863] transition-colors duration-1000 ease-[cubic-bezier(0.05,0,0,1)] text-2xl rounded-xl py-[7px] px-[38px] text-[#F5E2FF] cursor-pointer">
             {text}
           </button>
         </Link>
