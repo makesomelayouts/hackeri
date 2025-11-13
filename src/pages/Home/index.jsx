92ce84b666a7d277404278637a7aeaaf4ec7f46e
makesomelayouts <makesomelayouts@gmail.com>
2025-11-13 15:48:57 +0400

    Add man to the main page and start to adapt from pc

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,9 +3,9 @@ import Footer from "@widgets/Footer";
 import Header from "@widgets/Header";
 import arrow_down from "./assets/hero/arrow_down.png";
 import hero_intro from "./assets/hero/hero_intro.png";
-import ily from "./assets/welcome/anime_girl_ily.png";
 import circle from "./assets/welcome/circle.png";
 import ily_circle from "./assets/welcome/ily_circle.png";
+import man from "./assets/welcome/man.mp4";
 
 function Home() {
   const scrollToNextSection = () => {
@@ -17,41 +17,46 @@ function Home() {
 
   return (
     <>
-      <section className="snap-start h-dvh bg-[url(/src/pages/Home/assets/hero/hero_background.png)]">
+      <section className="max-h-dvh h-dvh md:pb-30 bg-[url(/src/pages/Home/assets/hero/hero_background.png)]">
         <Header />
-        <div className="flex flex-col justify-center items-center">
+
+        <div className="h-full flex flex-col justify-center items-center">
           <img
-            className="max-w-[1359.07px] max-h-[707.48px]"
+            className="lg:max-w-[80%] lg:max-h-[707.48px] md:w-[700px] sm:w-[500px]"
             src={hero_intro}
           />
           <img
-            className="max-h-[71px] cursor-pointer hover:animate-pulse duration-4000"
+            className="max-h-[71px] max-xl:w-10 max-md:w-8 max-sm:w-6 cursor-pointer hover:animate-pulse duration-4000"
             src={arrow_down}
             onClick={scrollToNextSection}
           />
         </div>
       </section>
 
-      <section className="snap-start h-dvh relative flex flex-col">
-        <div className="absolute bottom-20 right-0">
-          <img
-            src={ily_circle}
-            alt="Purple circle"
-            className="object-cover w-[1000px] h-[1000px] pointer-events-none select-none"
-          />
-          <img
-            src={ily}
-            alt="Girl Ily"
-            className="absolute bottom-0 right-0 w-full h-full mix-blend-color-dodge max-w-[625px] max-h-[837px] pointer-events-none select-none"
-          />
-        </div>
-
+      <section className="min-h-dvh flex flex-col bg-[#010207]">
         <img
           src={circle}
           className="absolute top-0 left-0 pointer-events-none select-none"
         />
-        <div className="max-w-[1140px] w-full mx-auto grow">
-          <p className="text-[#f5e2ff] text-[32px] mb-8 mt-[35px] max-w-[874px]">
+        <div className="relative max-w-[1140px] w-full mx-auto grow overflow-hidden">
+          <div className="absolute bottom-20 right-0 pointer-events-none select-none">
+            <img
+              src={ily_circle}
+              alt="Purple circle"
+              className="object-cover w-[1000px] h-[1000px] pointer-events-none select-none"
+            />
+            <video
+              src={man}
+              alt="Man"
+              className="absolute -bottom-86 -right-10 w-full h-full pointer-events-none select-none mix-blend-color-dodge md:max-w-[400px] 2xl:max-w-[500px]"
+              loop
+              autoPlay
+              muted
+              playsInline
+            />
+          </div>
+
+          <p className="text-[#f5e2ff] lg:text-xl max-xl:max-w-[200px] max-xl:text-2xl 2xl:text-[32px] mb-8 mt-[35px] xl:max-w-[874px]">
             Добро пожаловать в мир «Хацкеров» - настольной игры, где вы
             примерите на себя роль элитного киберпреступника, стремящегося к
             вершинам цифровой иерархии! Пробирайтесь сквозь слои защиты,
--- src/widgets/Header.jsx
+++ src/widgets/Header.jsx
@@ -3,10 +3,14 @@ import { Link } from "react-router";
 
 function Header() {
   return (
-    <header className="flex justify-center items-center gap-7 py-[33px] font-regular text-[32px] lowercase text-[#F5E2FF]">
+    <header className="flex max-md:hidden justify-center items-center gap-7 py-[33px] font-regular sm:text-xl md:text-2xl lg:text-[30px] xl:text-[32px] lowercase text-[#F5E2FF]">
       <Link to="/">Главная</Link>
       <Link to="/characters">Персонажи</Link>
-      <img src={logo} alt="Лого" className="w-[66.44px] h-[51px] mx-5" />
+      <img
+        src={logo}
+        alt="Лого"
+        className="lg:w-[66.44px] lg:h-[51px] w-7 h-7 mx-5"
+      />
       <Link to="/rules">Правила</Link>
       <Link to="/shop">Магазин</Link>
     </header>
