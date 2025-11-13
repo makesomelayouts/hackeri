97e8cf341e02320267772b4db92c6a72c994f3a3
makesomelayouts <makesomelayouts@gmail.com>
2025-11-13 13:29:35 +0400

    Hackeri forever! Auto update current year copyright

--- src/widgets/Footer.jsx
+++ src/widgets/Footer.jsx
@@ -1,8 +1,19 @@
 function Footer() {
+  const currentYear = new Date().getFullYear().toString();
+  console.log(currentYear);
+
   return (
     <footer className="py-[42px] px-4 bg-[#0D031B] z-10">
       <div className="max-w-[1140px] mx-auto">
-        <p className="text-[#f5e2ff] text-2xl">2025 © Хацкеры</p>
+        {currentYear === "2025" ? (
+          <p className="text-[#f5e2ff] text-2xl">2025 © Хацкеры</p>
+        ) : (
+          currentYear > "2025" && (
+            <p className="text-[#f5e2ff] text-2xl">
+              2025 — {currentYear} © Хацкеры
+            </p>
+          )
+        )}
       </div>
     </footer>
   );
