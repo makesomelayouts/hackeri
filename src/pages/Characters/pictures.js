08aab31de17d0a1191627be0475db1b2ccc6e2cb
makesomelayouts <makesomelayouts@gmail.com>
2025-11-13 12:21:16 +0400

    Add character pictures

--- src/pages/Characters/pictures.js
+++ src/pages/Characters/pictures.js
@@ -2,52 +2,123 @@
 import word from "@pages/Characters/assets/word.mp4";
 // access
 // paint
+import paint from "@pages/Characters/assets/paint.mp4";
 // pptx
 // excel
+import excel from "@pages/Characters/assets/excel.mp4";
 
 // viruses
 // chernobyl CIH
+import cih from "@pages/Characters/assets/cih.png";
 // ILOVEYOU
+import iloveyou from "@pages/Characters/assets/iloveyou.png";
 // trojan
+import trojan from "@pages/Characters/assets/trojan.png";
 // nimda
+import nimda from "@pages/Characters/assets/nimda.png";
 // wannacry
+import wannacry from "@pages/Characters/assets/wannacry.png";
 // melissa
+import melissa from "@pages/Characters/assets/melissa.png";
 // rootkit
+import rootkit from "@pages/Characters/assets/rootkit.png";
 // spyware
+import spyware from "@pages/Characters/assets/spyware.png";
 // adware
+import adware from "@pages/Characters/assets/adware.png";
 // zombie
+import zombie from "@pages/Characters/assets/zombie.png";
 // worm
+import worm from "@pages/Characters/assets/worm.png";
 
 // difficulties
 // infogypsy
+import infogypsy from "@pages/Characters/assets/infogypsy.mp4";
 // phishing link
+import phishingLink from "@pages/Characters/assets/phishing-link.mp4";
 // scam
 // ddos
 // advertising
+import advertising from "@pages/Characters/assets/advertising.mp4";
 // account hacked
+// import accountHacked from "@pages/Characters/assets/account-hacked.mp4";
 // mom burned out
+import momBurnedOut from "@pages/Characters/assets/mom-burned-out.mp4";
 // doxing
+import doxing from "@pages/Characters/assets/doxing.mp4";
 // blue screen
+import blueScreen from "@pages/Characters/assets/blue-screen.mp4";
 // spamming
+import spamming from "@pages/Characters/assets/spamming.mp4";
 
 // traps
 import noConnection from "@pages/Characters/assets/no-connection.png";
 // page not found
+import pageNotFound from "@pages/Characters/assets/page-not-found.png";
 // program crash
+import programCrash from "@pages/Characters/assets/program-crash.png";
 // maze of fear
+import mazeOfFear from "@pages/Characters/assets/maze-of-fear.png";
 // disk memory full
+import diskMemoryFull from "@pages/Characters/assets/disk-memory-full.png";
 
 // Memory cards
 // Autosave
+import autosave from "@pages/Characters/assets/autosave.png";
 // New processor
+import newProcessor from "@pages/Characters/assets/newProcessor.png";
 // Antivirus
+import antivirus from "@pages/Characters/assets/antivirus.png";
 // AdBlock
+import adblock from "@pages/Characters/assets/adblock.png";
 // System Administrator
+import systemAdministrator from "@pages/Characters/assets/system-administrator.png";
 
 const pictures = {
   word,
+  // access,
+  paint,
+  // pptx,
+  excel,
+
+  // viruses
+  cih,
+  iloveyou,
+  trojan,
+  nimda,
+  wannacry,
+  melissa,
+  rootkit,
+  spyware,
+  adware,
+  zombie,
+  worm,
+
+  // difficulties
+  infogypsy,
+  phishingLink,
+  // scam,
+  // ddos,
+  advertising,
+  // accountHacked,
+  momBurnedOut,
+  doxing,
+  blueScreen,
+  spamming,
+
+  // traps
   noConnection,
-  // Add other imports here as you uncomment them
+  pageNotFound,
+  programCrash,
+  mazeOfFear,
+  diskMemoryFull,
+
+  // memory cards
+  autosave,
+  newProcessor,
+  antivirus,
+  adblock,
+  systemAdministrator,
 };
 
 export default pictures;
