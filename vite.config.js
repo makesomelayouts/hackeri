443b3a855e88e06701b75715df84641e0f2e0146
makesomelayouts <makesomelayouts@gmail.com>
2025-11-13 12:17:25 +0400

    Fix vercel 0

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@app": "/src/app",
-      "@entities": "/src/entities",
-      "@features": "/src/features",
-      "@pages": "/src/pages",
-      "@shared": "/src/shared",
-      "@widgets": "/src/widgets",
+      "@app": path.resolve(__dirname, "src/app"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
     },
   },
 });
