209e65298df52ce7e959016b25076f0b343a721f
makesomelayouts <makesomelayouts@gmail.com>
2025-11-18 13:07:52 +0400

    Adapt main page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -20,36 +20,53 @@ function Home() {
       <section className="max-h-dvh h-dvh md:pb-30 bg-[url(/src/pages/Home/assets/hero/hero_background.png)]">
         <Header />
 
-        <div className="h-full flex flex-col justify-center items-center">
+        <div className="h-full flex flex-col justify-center items-center pb-4">
           <img
-            className="lg:w-[850px] md:w-[700px] sm:w-[500px] max-[646px]:w-[90%]"
+            className="xl:w-[70%] lg:w-[850px] md:w-[700px] sm:w-[500px] max-[646px]:w-[90%]"
             src={hero_intro}
           />
           <img
-            className="w-[5%] max-h-[71px] cursor-pointer hover:animate-pulse duration-4000"
+            className="max-[400px]:w-[18px] w-[24px] sm:w-[24px] md:w-[30px] lg:w-[36px] xl:w-[42px] 2xl:w-[46px] max-h-[71px] cursor-pointer hover:animate-pulse duration-4000"
             src={arrow_down}
             onClick={scrollToNextSection}
           />
         </div>
       </section>
 
-      <section className="min-h-dvh flex flex-col bg-[#010207]">
+      <section className="overflow-hidden relative h-full max-[767px]:min-h-[600px] md:min-h-dvh flex flex-col bg-[#010207]">
         <img
           src={circle}
-          className="absolute top-0 left-0 pointer-events-none select-none"
+          className="absolute top-70 left-0 w-[1000px] pointer-events-none select-none"
         />
+
+        <div className="max-lg:hidden absolute bottom-38 right-0 pointer-events-none select-none">
+          <img
+            src={ily_circle}
+            alt="Purple circle"
+            className="object-cover w-[1000px] h-[1000px] pointer-events-none select-none"
+          />
+          <video
+            src={man}
+            alt="Man"
+            className="absolute lg:-bottom-8 xl:bottom-0 right-28 w-[60%] pointer-events-none select-none mix-blend-color-dodge"
+            loop
+            autoPlay
+            muted
+            playsInline
+          />
+        </div>
+
         <div className="px-4 relative max-w-[1140px] w-full mx-auto grow overflow-hidden">
-          <div className="absolute max-md:-bottom-26 md:-bottom-44 right-0 pointer-events-none select-none">
+          <div className="lg:hidden absolute max-md:-bottom-26 md:-bottom-44 right-0 pointer-events-none select-none">
             <img
               src={ily_circle}
               alt="Purple circle"
-              className="object-cover w-[1000px] h-[1000px] pointer-events-none select-none"
+              className="object-cover w-[400px] h-[400px] pointer-events-none select-none"
             />
-            {/* lg:-bottom-86*/}
             <video
               src={man}
               alt="Man"
-              className="max-[332px]:bottom-0 absolute bottom-22 right-0 max-md:right-8 max-md:w-40 md:w-80 xl:w-full xl:h-full pointer-events-none select-none mix-blend-color-dodge"
+              className="max-[332px]:bottom-18 absolute bottom-22 right-0 max-md:right-8 max-md:w-40 md:w-80 pointer-events-none select-none mix-blend-color-dodge"
               loop
               autoPlay
               muted
@@ -57,7 +74,7 @@ function Home() {
             />
           </div>
 
-          <p className="text-[#f5e2ff] max-sm:text-[12px] sm:text-sm lg:text-xl xl:text-2xl 2xl:text-[32px] mb-8 mt-[35px] lg:max-w-[200px] xl:max-w-[874px]">
+          <p className="text-[#f5e2ff] max-sm:text-[12px] sm:text-sm lg:text-xl xl:text-2xl 2xl:text-[32px] mb-8 mt-[35px] md:max-w-[600px] lg:max-w-[874px]">
             Добро пожаловать в мир «Хацкеров» - настольной игры, где вы
             примерите на себя роль элитного киберпреступника, стремящегося к
             вершинам цифровой иерархии! Пробирайтесь сквозь слои защиты,
@@ -65,7 +82,7 @@ function Home() {
             взламывайте сайты и собирайте крутые ништяки, чтобы стать самым
             богатым и влиятельным хакером в сети!
           </p>
-          <div className="flex flex-wrap gap-[29px]">
+          <div className="flex flex-wrap gap-[29px] pb-8 max-md:pb-52">
             <Button text="персонажи" link="characters" />
             <Button text="правила" link="rules" />
           </div>
