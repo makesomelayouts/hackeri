7d677586a1c5e212f314acb03a51a3d784149f9c
makesomelayouts <makesomelayouts@gmail.com>
2025-11-22 20:27:56 +0400

    Adapt footer

--- src/widgets/Footer.jsx
+++ src/widgets/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
 
   return (
     <footer className="py-6 md:py-[42px] px-4 bg-[#0D031B] z-10">
-      <div className="max-w-[1140px] mx-auto flex flex-wrap justify-between items-center">
+      <div className="max-md:flex-col max-w-[1140px] mx-auto flex flex-wrap justify-between items-center">
         {currentYear === "2025" ? (
           <p className="text-[#f5e2ff] max-sm:text-[12px] sm:text-sm lg:text-xl xl:text-2xl 2xl:text-[32px]">
             2025 © Хацкеры
@@ -19,21 +19,42 @@ function Footer() {
         {/* links */}
         <p className="py-2 text-[#f5e2ff] max-sm:text-[12px] sm:text-sm lg:text-xl xl:text-2xl 2xl:text-[32px]">
           tg: (
-          <a href="https://t.me/qwkenk" target="_blank">
-            @qwkenk
-          </a>
-          ,&ensp;
-          <a href="https://t.me/lizergin_xtra" target="_blank">
-            @lizergin_xtra
-          </a>
-          ,&ensp;
-          <a href="https://t.me/witchblades_draw" target="_blank">
-            @witchblades_draw
-          </a>
-          ,&ensp;
-          <a href="https://t.me/makesomelayouts" target="_blank">
-            @makesomelayouts
-          </a>
+          <div className="md:inline max-md:leading-6">
+            <a
+              className="hover:text-[#BC13FE] duration-300 ease-in"
+              href="https://t.me/qwkenk"
+              target="_blank"
+            >
+              @qwkenk
+            </a>
+            ,&ensp;
+            <br className="md:hidden" />
+            <a
+              className="hover:text-[#BC13FE] duration-300 ease-in"
+              href="https://t.me/witchblades_draw"
+              target="_blank"
+            >
+              @witchblades_draw
+            </a>
+            ,&ensp;
+            <br className="md:hidden" />
+            <a
+              className="hover:text-[#BC13FE] duration-300 ease-in"
+              href="https://t.me/makesomelayouts"
+              target="_blank"
+            >
+              @makesomelayouts
+            </a>
+            ,&ensp;
+            <br className="md:hidden" />
+            <a
+              className="hover:text-[#BC13FE] duration-300 ease-in"
+              href="https://t.me/lizergin_xtra"
+              target="_blank"
+            >
+              @lizergin_xtra
+            </a>
+          </div>
           )
         </p>
       </div>
