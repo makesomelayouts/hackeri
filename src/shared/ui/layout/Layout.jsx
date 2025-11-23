3b8bf7d4b01d71364511963c1445860a4b9e7b7a
makesomelayouts <makesomelayouts@gmail.com>
2025-11-23 22:17:10 +0400

    Add scroll restoration, to always be on the top of the page, when opens page

--- src/shared/ui/layout/Layout.jsx
+++ src/shared/ui/layout/Layout.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router";
+import { Outlet, ScrollRestoration } from "react-router";
 
 function Layout() {
   return (
     <main className="bg-[#010207] flex flex-col font-unbounded">
       {/* <div className="h-dvh snap-y snap-mandatory overflow-y-auto">*/}
       <Outlet />
+      <ScrollRestoration />
       {/* </div>*/}
     </main>
   );
