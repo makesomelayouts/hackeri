74e3c6931cd66d66341dca9772b54fbe249608ab
makesomelayouts <makesomelayouts@gmail.com>
2025-12-02 19:28:51 +0400

    Remove __dirname warning as is not defined

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+
+// to remove __dirname warning as is not defined
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
